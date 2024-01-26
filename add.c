96e228bbcb9e317b6c62222960bba80e871355dc
vraj2711 <156588583+vraj2711@users.noreply.github.com>
2024-01-27 00:09:39 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(){
+    int a, b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    int c=a+b;
+    printf("Sum %d", c);
+    return 0;
+
+}
